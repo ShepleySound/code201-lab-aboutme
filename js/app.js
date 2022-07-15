d33a4548610527cae2a3c2bff7d1a2fee91c5c96
Robert Shepley <shepleysound@gmail.com>
2022-07-15 09:56:21 -0700

    Refactor first five questions to single looping function.

--- js/app.js
+++ js/app.js
@@ -17,12 +17,17 @@ startButton.addEventListener('click', () => {
 
   alert('The following questions should be answered with a simple \'yes\' or \'no\'');
 
+  let fiveQuestions = [
+    ['Am I in the Air Force?', false, 'That\'s correct! I\'m in the Army.', 'No, that\'s not correct.'],
+    ['Have I watched every Star Wars film/show in chronological order?', true, 'Yeah... That took a while.', 'I appreciate your faith in me, but I have...'],
+    ['I used to play the euphonium. Do you know what that is?', true, 'Good answer! You must know a lot about useless things.', 'Wrong answer... Google it and come back, please.'],
+    ['Do I have a Lord of the Rings tattoo?', true, 'I do! It\'s the broken sword Narsil, on my right forearm.', 'Sorry, that is not correct.'],
+    ['Do I have any dogs?', false, 'Correct! Maybe soon, though. My cats would like a new friend.', 'Nope, I just have two cats. One of them hangs out on my desk sometimes.']
+  ];
+
   points = 0;
-  question1();
-  question2();
-  question3();
-  question4();
-  question5();
+
+  trueOrFalseQuestions(fiveQuestions);
   question6(4);
   question7(6);
 
@@ -35,145 +40,45 @@ startButton.addEventListener('click', () => {
   pointAmount.innerText = `${points}`;
 });
 
-function question1(){
-  let isAnswered = false;
-  while (!isAnswered) {
-    let answerOne = prompt('Am I in the Air Force?');
-    switch (answerOne.toLowerCase()) {
-    case 'y':
-    case 'yes':
-    case 'true':
-      // console.log('Incorrect');
-      alert('No, that\'s not correct.');
-      isAnswered = true;
-      break;
-    case 'n':
-    case 'no':
-    case 'false':
-      // console.log('Correct');
-      alert('That\'s correct! I\'m in the Army.');
-      points++;
-      isAnswered = true;
-      break;
-    default:
-      alert('Please enter either y/yes/true or n/no/false');
-      break;
+function trueOrFalseQuestions(questionsDataArray){
+  let answerBool;
+  questionsDataArray.forEach(questionData => {
+    let isAnswered = false;
+    while (!isAnswered) {
+      answerBool
+      let answer = prompt(questionData[0]);
+      switch (answer.toLowerCase()) {
+      case 'y':
+      case 'yes':
+      case 'true':
+        isAnswered = true;
+        answerBool = true;
+        break;
+      case 'n':
+      case 'no':
+      case 'false':
+        isAnswered = true;
+        answerBool = false;
+        break;
+      default:
+        alert('Please enter either y/yes/true or n/no/false');
+        break;
+      }
     }
-  }
-}
-
-function question2(){
-  let isAnswered = false;
-  while (!isAnswered) {
-    let answerTwo = prompt('Have I watched every Star Wars film/show in chronological order?');
-    switch (answerTwo.toLowerCase()) {
-    case 'y':
-    case 'yes':
-    case 'true':
-      // console.log('Correct');
-      alert('Yeah... That took a while.');
+    if (answerBool === questionData[1]) {
+      alert(questionData[2]);
       points++;
-      isAnswered = true;
-      break;
-    case 'n':
-    case 'no':
-    case 'false':
-      // console.log('Incorrect');
-      alert('I appreciate your faith in me, but I have...');
-      isAnswered = true;
-      break;
-    default:
-      alert('Please enter either y/yes/true or n/no/false');
-      break;
+    } else {
+      alert(questionData[3]);
+      points++
     }
-  }
-}
-
-function question3(){
-  let isAnswered = false;
-  while (!isAnswered) {
-    let answerThree = prompt('I used to play the euphonium. Do you know what that is?');
-    switch (answerThree.toLowerCase()) {
-    case 'y':
-    case 'yes':
-    case 'true':
-      // console.log('Correct');
-      alert('Good answer! You must know a lot about useless things.');
-      points++;
-      isAnswered = true;
-      break;
-    case 'n':
-    case 'no':
-    case 'false':
-      // console.log('Incorrect');
-      alert('Wrong answer... Google it and come back, please.');
-      isAnswered = true;
-      break;
-    default:
-      alert('Please enter either y/yes/true or n/no/false');
-      break;
-    }
-  }
-}
-
-function question4(){
-  let isAnswered = false;
-  while (!isAnswered) {
-    let answerFour = prompt('Do I have a Lord of the Rings tattoo?');
-    switch (answerFour.toLowerCase()) {
-    case 'y':
-    case 'yes':
-    case 'true':
-      // console.log('Correct');
-      alert('I do! It\'s the broken sword Narsil, on my right forearm.');
-      points++;
-      isAnswered = true;
-      break;
-    case 'n':
-    case 'no':
-    case 'false':
-      // console.log('Incorrect');
-      alert('Sorry, that is not correct.');
-      isAnswered = true;
-      break;
-    default:
-      alert('Please enter either y/yes/true or n/no/false');
-      break;
-    }
-  }
-}
-
-function question5(){
-  let isAnswered = false;
-  while (!isAnswered) {
-    let answerFive = prompt('Do I have any dogs?');
-    switch (answerFive.toLowerCase()) {
-    case 'y':
-    case 'yes':
-    case 'true':
-      // console.log('Incorrect');
-      alert('Nope, I just have two cats. One of them hangs out on my desk sometimes.');
-      isAnswered = true;
-      break;
-    case 'n':
-    case 'no':
-    case 'false':
-      // console.log('Correct');
-      alert('Correct! Maybe soon, though. My cats would like a new friend.');
-      points++;
-      isAnswered = true;
-      break;
-    default:
-      alert('Please enter either y/yes/true or n/no/false');
-      break;
-    }
-  }
+  });
 }
 
 function question6(guessesRemaining){
   let isAnswered = false;
   while (!isAnswered && guessesRemaining) {
-    let answerSix = prompt('How many years old am I?');
+    let answerSix = prompt('How many years old am I? (Enter a number)');
     let answerInt = parseInt(answerSix);
     if (answerSix === null) {
       break;
@@ -204,7 +109,7 @@ function question7(guessesRemaining){
   let isAnswered = false;
   let possibilities = ['Myrtle Beach', 'Columbia', 'Baltimore', 'El Paso', 'Federal Way', 'Tacoma'];
   while (!isAnswered && guessesRemaining) {
-    let answerSeven = prompt('Guess a city that I\'ve lived in (or currently live in).');
+    let answerSeven = prompt('Guess a city that I\'ve lived in (or currently live in). (Enter a city name)');
     possibilities.forEach((element, i) => {
       if (element.toLowerCase() === answerSeven.toLowerCase()) {
         points++;
